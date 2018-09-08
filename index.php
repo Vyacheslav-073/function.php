4b4f49e03eab8e3a44a1b0abd774198b22ca7cad
Vyacheslav <viacheslav.suprun@yandex.ru>
2018-09-08 19:02:12 +0300

    function.php

--- index.php
+++ index.php
@@ -2,18 +2,20 @@
 
     function greeting($name = "имя", $weekday = 0) {
 
-        switch ($weekday <= 5) {
-            
-            case true:
-            
-                echo "Привет, $name! Хорошего и продуктивного рабочего дня!";
-                break;
-            
-            default:
-            
-                echo "Привет $name! Желаю вам хорошо отдохнуть в этот день!";
-                break; 
+        if ($weekday <= 5) {
+            echo "Привет $name! Желаю вам хорошо отдохнуть в этот день!";
+        } else {
+            switch($weekday){
+                case 6; case 7;
+                    echo "Привет, $name! Хорошего и продуктивного рабочего дня!";
+                    break;
+                    
+                default:
+                    echo "Привет $name! Введите правильное значение!";
+                    break;     
+            }
         }
+            
     }
 
     greeting("Александр", 1);
